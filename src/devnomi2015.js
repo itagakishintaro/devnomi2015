18fce700cf98db96e3b1ab9ddbfa8f9af250e3af
itagaki <itagaki.shintaro@gmail.com>
2015-05-18 14:59:07 +0900

    問題を追加

--- src/devnomi2015.js
+++ src/devnomi2015.js
@@ -1,6 +1,39 @@
 /**
+ * 回文判定
+ * 配点: 2点
+ * 入力: 文字列
+ * 出力: 回文であればtrue, そうでなければfalse
+ */
+function isPalindrome(str) {
+  return str.split("").reverse().join("") === str;
+}
+
+/**
+ * 輪になって並ぶ
+ * 先生のいる位置を{ x: 0, y: 0 }として、生徒の数と半径に応じて、生徒を先生を中心に円状に均等に配置する。
+ * 配点: 4点
+ * 条件: 入力値は2つとも必ず1以上の整数としてよい
+ * 入力: 生徒の数、半径
+ * 出力: オブジェクトの配列
+ *       - 1人目はxの正方向
+ *       - 反時計回り
+ *       - 小数は整数に四捨五入
+ *       例：[ { x: 10, y: 0 }, { x: 0, y: 10 }, { x: -10, y: 0 }, { x: 0, y: -10 } ]
+ */
+function standCiecle(studentNum, radius) {
+  var positions = [];
+  for(var i = 0; i < studentNum; i++){
+    positions.push({
+      x: Math.round( radius * Math.cos( (i / studentNum * 360) / 180 * Math.PI ) ),
+      y: Math.round( radius * Math.sin( (i / studentNum * 360) / 180 * Math.PI ) )
+    });
+  }
+  return positions;
+}
+
+/**
  * 数値を音階に変換する
- * 配点: 5点
+ * 配点: 4点
  * 条件: 入力は必ず要素を1つ以上持ち、値は必ず0以上の数値としてよい
  * 入力: 数値の配列(数は不定)
  * 出力: 数値を次のとおり変換した配列
@@ -48,3 +81,78 @@ function tetris(blocks) {
   }
   return newBlocks
  }
+
+/**
+ * BMIが最も高い人を探す
+ * 配点: 10点
+ * 条件: 入力のバリデーションは不要
+ * 入力: [ {id: 1, height: 170, weight: 80}, {id: 2, height: 160, weight: 50} ]のようなオブジェクトの配列
+ *   - 身長はcm
+ *   - 体重はkg
+ * 出力: [1]のようなidの配列
+ *   - BMIは 体重(kg) / ( 身長(m) * 身長(m) )。身長はcmではなく、mなので注意。
+ *   - 最大BMIの人が複数いる場合は複数返す
+ */
+function searchMaxBMIPersons(persons) {
+  var maxBMI = Math.max.apply(null, persons.map(function(v) {
+    return bmi(v.height, v.weight);
+  }));
+
+  return persons.filter(function(v){
+    return bmi(v.height, v.weight) === maxBMI;
+  }).map(function(v){
+    return v.id;
+  });
+
+  function bmi(h, w){
+    return w / (h / 100 * h / 100);
+  }  
+}
+
+ /**
+ * ボーリングのスコア計算
+ * 配点: 30点
+ * 条件: 入力のバリデーションは不要
+ * 入力: [ [9, 1], [10], [4, 1], [3] ]のように配列が入力される
+ *   - 1フレームごとに[9, 1]のような配列を持つ
+ *   - ストライクのときは[10]
+ *   - [3]のようなときはまだ2投目を投げていない
+ * 出力: [ 10, 15, 5 ]のように配列が出力される
+ */
+function countBowlingScore(input) {
+  var score = []
+  input.forEach(function(v, i){
+    // フレームが完了しておらず、点数未定の場合
+    if( v[0] != 10 && v[1] === undefined ){
+      return;
+    }
+    // ストライクの場合
+    else if( v[0] == 10 ){
+      // ストライクでまだ点数未定の場合 
+      if( ( input[i+1] === undefined ? 0 : input[i+1].length )
+           + ( input[i+2] === undefined ? 0 : input[i+2].length ) < 2 ){
+        return;
+      }
+      // ダブルかターキーの場合
+      else if( input[i+1][0] === 10 ){
+        score.push( 20 + input[i+2][0] );
+      } else {
+        score.push( 10 + input[i+1][0] + input[i+1][1] );
+      }
+    }
+    // スペアの場合
+    else if( v[0] + v[1] == 10 ){
+      // スペアでまだ点数未定の場合
+      if( input[i+1] === undefined ){
+        return;
+      } else {
+        score.push( 10 + input[i+1][0] );
+      }
+    }
+    // その他の場合
+    else {
+      score.push( v[0] + v[1] );
+    }
+  });  
+  return score;
+}
--- src/devnomi2015Test.js
+++ src/devnomi2015Test.js
@@ -1,12 +1,49 @@
-QUnit.test("問題1:数値を音階に変換する", function(assert) {
+QUnit.test("回文判定-true", function(assert) {
     // setup
-    var input = [0, 99.9, 400, 499.99, 500, 599, 450, 800, 300, 399, 200, 299, 100, 199, 50.123];
+    var str = "たけやぶやけた";
+    // expoect
+    assert.ok(isPalindrome(str));
+});
+QUnit.test("回文判定-false", function(assert) {
+    // setup
+    var str = "かいぶんなのか";
+    // expoect
+    assert.notOk(isPalindrome(str));
+});
+
+QUnit.test("輪になって並ぶ-1", function(assert) {
+    // setup
+    var studentNum = 4;
+    var radius = 10;
+    var expected = [ { x: 10, y: 0 }, { x: 0, y: 10 }, { x: -10, y: 0 }, { x: 0, y: -10 } ];
+    // expoect
+    assert.deepEqual(standCiecle(studentNum, radius), expected);
+});
+QUnit.test("輪になって並ぶ-2", function(assert) {
+    // setup
+    var studentNum = 8;
+    var radius = 10;
+    var expected = [ { x: 10, y: 0 }, { x: 7, y: 7 }, { x: 0, y: 10 }, { x: -7, y: 7 }, { x: -10, y: 0 }, { x: -7, y: -7 }, { x: 0, y: -10 }, { x: 7, y: -7 } ];
+    // expoect
+    assert.deepEqual(standCiecle(studentNum, radius), expected);
+});
+
+QUnit.test("数値を音階に変換する-1", function(assert) {
+    // setup
+    var input = [0, 99, 400, 499, 500, 599, 450, 800, 300, 399, 200, 299, 100, 199, 50];
+    var expected = ["C3", "C3", "G3", "G3", "H3", "H3", "G3", "", "F3", "F3", "E3", "E3", "D3", "D3", "C3"];
+    // expect
+    assert.deepEqual(num2octave(input), expected);
+});
+QUnit.test("数値を音階に変換する-2", function(assert) {
+    // setup
+    var input = [0.0, 99.9, 400.0, 499.99, 500.0, 599.99, 450.0, 800.0, 300.0, 399.99, 200.0, 299.99, 100.0, 199.99, 50.1234567890];
     var expected = ["C3", "C3", "G3", "G3", "H3", "H3", "G3", "", "F3", "F3", "E3", "E3", "D3", "D3", "C3"];
     // expect
     assert.deepEqual(num2octave(input), expected);
 });
 
-QUnit.test("問題2:テトリスのブロック崩し", function(assert) {
+QUnit.test("テトリスのブロック崩し-1", function(assert) {
     // setup
     var base = [];
     for(var i = 0; i < 20; i++){
@@ -21,4 +58,129 @@ QUnit.test("問題2:テトリスのブロック崩し", function(assert) {
     expected[0] = [1, 1, 1, 1, 1, 0, 1, 1, 1, 1];
     // expect
     assert.deepEqual(tetris(input), expected);
+});
+QUnit.test("テトリスのブロック崩し-2", function(assert) {
+    // setup
+    var base = [];
+    for(var i = 0; i < 20; i++){
+      base.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    }
+    var input = base.concat();
+    input[2] = [1, 1, 1, 0, 0, 0, 0, 0, 1, 1];
+    input[1] = [1, 1, 1, 1, 1, 0, 1, 1, 1, 1];
+    input[0] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    var expected = base.concat();
+    expected[1] = [1, 1, 1, 0, 0, 0, 0, 0, 1, 1];
+    expected[0] = [1, 1, 1, 1, 1, 0, 1, 1, 1, 1];
+    // expect
+    assert.deepEqual(tetris(input), expected);
+});
+
+QUnit.test("BMIが最も高い人を探す-1", function(assert) {
+    // setup
+    persons = [ {id: 1, height: 170, weight: 80}
+             ,{id: 2, height: 160, weight: 50}
+             ,{id: 3, height: 180, weight: 90} ];
+    // expect
+    assert.deepEqual(searchMaxBMIPersons(persons), [3]);
+});
+QUnit.test("BMIが最も高い人を探す-2", function(assert) {
+    // setup
+    persons = [ {id: 1, height: 170, weight: 80}
+             ,{id: 2, height: 160, weight: 50}
+             ,{id: 3, height: 180, weight: 100}
+             ,{id: 4, height: 162, weight: 81} ];
+    // expect
+    assert.deepEqual(searchMaxBMIPersons(persons), [3, 4]);
+});
+
+QUnit.test("ボーリングのスコア計算-点数なし1", function(assert) {
+    // setup
+    input = [];
+    // expect
+    assert.deepEqual([], countBowlingScore(input));
+});
+QUnit.test("ボーリングのスコア計算-点数なし2", function(assert) {
+    // setup
+    input = [[1]];
+    // expect
+    assert.deepEqual([], countBowlingScore(input));
+});
+QUnit.test("ボーリングのスコア計算-点数あり1", function(assert) {
+    // setup
+    input = [[0, 0]];
+    // expect
+    assert.deepEqual([0], countBowlingScore(input));
+});
+QUnit.test("ボーリングのスコア計算-点数あり2", function(assert) {
+    // setup
+    input = [[3, 6], [9]];
+    // expect
+    assert.deepEqual([9], countBowlingScore(input));
+});
+QUnit.test("ボーリングのスコア計算-スペア1", function(assert) {
+    // setup
+    input = [[9, 1]];
+    // expect
+    assert.deepEqual([], countBowlingScore(input));
+});
+QUnit.test("ボーリングのスコア計算-スペア2", function(assert) {
+    // setup
+    input = [[0, 10], [9]];
+    // expect
+    assert.deepEqual([19], countBowlingScore(input));
+});
+QUnit.test("ボーリングのスコア計算-ストライク1", function(assert) {
+    // setup
+    input = [[10]];
+    // expect
+    assert.deepEqual([], countBowlingScore(input));
+});
+QUnit.test("ボーリングのスコア計算-ストライク2", function(assert) {
+    // setup
+    input = [[10], [9]];
+    // expect
+    assert.deepEqual([], countBowlingScore(input));
+});
+QUnit.test("ボーリングのスコア計算-ストライク3", function(assert) {
+    // setup
+    input = [[10], [9, 1]];
+    // expect
+    assert.deepEqual([20], countBowlingScore(input));
+});
+QUnit.test("ボーリングのスコア計算-ダブル1", function(assert) {
+    // setup
+    input = [[10], [10]];
+    // expect
+    assert.deepEqual([], countBowlingScore(input));
+});
+QUnit.test("ボーリングのスコア計算-ダブル2", function(assert) {
+    // setup
+    input = [[10], [10], [9]];
+    // expect
+    assert.deepEqual([29], countBowlingScore(input));
+});
+QUnit.test("ボーリングのスコア計算-ダブル3", function(assert) {
+    // setup
+    input = [[10], [10], [9, 1]];
+    // expect
+    assert.deepEqual([29, 20], countBowlingScore(input));
+});
+QUnit.test("ボーリングのスコア計算-トリプル1", function(assert) {
+    // setup
+    input = [[10], [10], [10]];
+    // expect
+    assert.deepEqual([30], countBowlingScore(input));
+});
+QUnit.test("ボーリングのスコア計算-トリプル2", function(assert) {
+    // setup
+    input = [[10], [10], [10], [9]];
+    // expect
+    assert.deepEqual([30, 29], countBowlingScore(input));
+});
+QUnit.test("ボーリングのスコア計算-トリプル3", function(assert) {
+    // setup
+    input = [[10], [10], [10], [9, 1]];
+    // expect
+    assert.deepEqual([30, 29, 20], countBowlingScore(input));
 });
